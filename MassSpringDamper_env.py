148b914a3d1448262725991538cb6121483cec33
Vilde Gjærum <vildegjaerum@gmail.com>
2020-03-04 08:56:30 +0100

    Update MassSpringDamper_env.py

--- MassSpringDamper_env.py
+++ MassSpringDamper_env.py
@@ -15,23 +15,23 @@ class MassSpringDamperEnv(gym.Env):
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second' : 50
     }
-	def __init__(self): #, spring_stiffness, damper_factor, mass, max_force
-		self.spring_stiffness = 1.5 #spring_stiffness
-		self.damper_factor = 6 #damper_factor
-		self.mass = 1.5 #mass
-		self.max_force = np.array([2]) #max_force
-		self.step_length = 0.15 # in seconds
-
+	def __init__(self): #, spring_stiffness, damper_factor, mass, max_force4
 		self.x_treshold = 4.8
-		obs_high = np.array([self.x_treshold,np.finfo(np.float32).max])
+		self.spring_stiffness = 0.5 #spring_stiffness
+		self.damper_factor = 0.25 #damper_factor
+		self.mass = 1 #mass
+		self.max_force = np.array([1]) #max_force
+		self.step_length = 0.1 # in seconds
+
+		obs_high = np.array([self.x_treshold,np.finfo(np.float32).max])#,self.x_treshold,np.finfo(np.float32).max])
 
 		self.observation_space = spaces.Box(-obs_high,obs_high, dtype=np.float32)
 		self.action_space = spaces.Box(-self.max_force,self.max_force,dtype=np.float32)
-
 		self.state = None # Some random init here?
 		self.goal_state = None  # Some position with no velocity
 
 		self.viewer = None
+		self.steps_taken = None
 		self.steps_beyond_done = None
 
 
@@ -40,35 +40,62 @@ class MassSpringDamperEnv(gym.Env):
 		state = self.state
 		x, x_dot = state
 		acceleration = (-self.spring_stiffness*x -self.damper_factor*x_dot + action)/self.mass
-		x_dot = acceleration*self.step_length
-		x = x_dot*self.step_length
-		self.state = (x,x_dot)
-		done = False
-		if x == self.goal_state[0] and x_dot == self.goal_state[1]:
-			done = True
-		if x < -self.x_treshold or x > self.x_treshold:
-			done = True
 
-		if not done:
-			reward = -np.abs(x - self.goal_state[0])**2 - np.abs(x_dot - self.goal_state[1])**2
-		elif self.steps_beyond_done is None:
-			self.steps_beyond_done = 0
-			reward = -np.abs(x - self.goal_state[0])**2 - np.abs(x_dot - self.goal_state[1])**2
+		x_dot += acceleration*self.step_length
+		x += x_dot*self.step_length
+		self.state = (x[0],x_dot[0]) #!!
+
+		x_goal,x_dot_goal = self.goal_state
+		done = False
+		reward = -np.abs(x - x_goal)**2
+
+		#print("Reward = -(",x,"-",x_goal,"^2 - (" ,x_dot," -" ,x_dot_goal,")^2 = ", reward)
+		if (np.abs(x- x_goal) < 0.1):# and (np.abs(x_dot -x_dot_goal)< 0.01):
+			#print("YAY! Managed to arrive at righ.")
+			reward = 1
+			if (np.abs(x_dot -x_dot_goal)< 0.1):
+				reward = 100
+				if (np.abs(x_dot -x_dot_goal)< 0.01):
+
+					#print("Getting there")
+					reward = 500
+					if (np.abs(x_dot -x_dot_goal)< 0.005):
+						reward = 1000
+						done = True
+						#print("YAY! Stopped at right position - ",x,x_dot)
+			#return np.array(self.state), reward, done,  {}
+
+		if x < -self.x_treshold or x > self.x_treshold:
+			reward = -100
+			done = True
+		if done:
+			if self.steps_beyond_done is None:
+				self.steps_beyond_done = 0
+			else:
+				if self.steps_beyond_done == 0:
+					logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
+					self.steps_beyond_done += 1
+					reward = 0.0
+		if self.steps_taken == None:
+			self.steps_taken = 1
 		else:
-			if self.steps_beyond_done == 0:
-				logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
-			self.steps_beyond_done += 1
-			reward = 0.0
+			self.steps_taken += 1
+			if self.steps_taken == 200:
+				self.done = True
+				done = True
+				self.steps_taken = None
 
 		return np.array(self.state), reward, done,  {}
 
-	def reset(self,goal_x=0.0,goal_x_dot=0.0):
-		self.state = (np.random.uniform(low = -self.x_treshold, high = self.x_treshold),np.random.uniform(low = -0.1, high = 0.1))
+	def reset(self,goal_x=-2.0,goal_x_dot=0.0):
+		self.state = (np.random.uniform(low = -self.x_treshold/2.0, high = self.x_treshold/2.0),np.random.uniform(low = -0.01, high = 0.01))
+
 		self.steps_beyond_done = None
 		self.goal_state =(goal_x,goal_x_dot)
 		return np.array(self.state)
 
 	def render(self, mode='human'):
+		print("Rendering")
 		screen_width = 600
 		screen_height = 400
 
@@ -88,9 +115,24 @@ class MassSpringDamperEnv(gym.Env):
 			self.mass_trans = rendering.Transform()
 			mass.add_attr(self.mass_trans)
 			self.viewer.add_geom(mass)
+
 			self.track = rendering.Line((0,mass_y), (screen_width,mass_y))
 			self.track.set_color(0,0,0)
 			self.viewer.add_geom(self.track)
+
+			### ADD SPRING
+			#p1 = [l,(t-b)/2.0]
+			#x = self.state
+			#mass_x = x[0]*scale+screen_width/2.0
+			#left = mass_x - mass_width/2.0
+			#y_m = mass_y + 0.25*mass_height
+			#y_t = mass_y + 0.5*mass_height
+			#y_b = mass_y
+
+			#self.p1 = rendering.Line(((3/4)*left,y_m),(left,y_m))
+			#self.p1.set_color(100,0,0)
+			#self.viewer.add_geom(self.p1)
+
 
 		if self.state is None: return None
 
